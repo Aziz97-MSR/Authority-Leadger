5ba57d885fad2928817894e8bf12ee6d1224077b
Prithibee13 <azizprithibee1213@gmail.com>
2026-04-08 22:54:30 +0600

    Footer styling

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,10 +1,10 @@
 const Footer = () => {
     return (
-        <footer className="mx-[64px] px-[32px]">
-            <article className="grid grid-cols-1 lg:grid-cols-4 lg:gap-[48px]">
+        <footer className="bg-[#fff] py-[64px] px-[32px]">
+            <article className="my-[48px] grid grid-cols-1 lg:grid-cols-4 lg:gap-[48px]">
                 <div>
-                    <h2 className="text-xl font-bold mb-5">Authority Ledger</h2>
-                    <p>
+                    <h2 className="text-[18px] font-semibold mb-5">Authority Ledger</h2>
+                    <p className="text-[#64748B] text-[14px] w-[268px] text-[14px]">
                         The premier choice for high-growth
                         enterprise marketing and digital
                         architecture.
@@ -12,39 +12,39 @@ const Footer = () => {
                 </div>
 
                 <div>
-                    <h2 className="text-xl font-bold mb-5">Company</h2>
+                    <h2 className="text-[16px] font-semibold mb-5">Company</h2>
 
-                    <p className="mb-5">About Us</p>
-                    <p className="mb-5">Case Studies</p>
-                    <p>Careers</p>
+                    <p className="text-[#64748B] text-[14px] mb-5">About Us</p>
+                    <p className="text-[#64748B] text-[14px] mb-5">Case Studies</p>
+                    <p className="text-[#64748B] text-[14px]">Careers</p>
                 </div>
 
                 <div>
                     
-                    <h2 className="text-xl font-bold mb-5">Services</h2>
+                    <h2 className="text-[16px] font-semibold mb-5">Services</h2>
 
-                    <p className="mb-5">Performance SEO</p>
-                    <p className="mb-5">Paid Media</p>
-                    <p>Brand Identity</p>
+                    <p className="text-[#64748B] text-[14px] mb-5">Performance SEO</p>
+                    <p className="text-[#64748B] text-[14px] mb-5">Paid Media</p>
+                    <p className="text-[#64748B] text-[14px]">Brand Identity</p>
                 </div>
 
                 <div>
                     
-                    <h2 className="text-xl font-bold mb-5">Connect</h2>
+                    <h2 className="text-[16px] font-semibold mb-5">Connect</h2>
 
-                    <p className="mb-5">LinkedIn</p>
-                    <p className="mb-5">Instagram</p>
-                    <p>Twitter (X)</p>
+                    <p className="text-[#64748B] text-[14px] mb-5">LinkedIn</p>
+                    <p className="text-[#64748B] text-[14px] mb-5">Instagram</p>
+                    <p className="text-[#64748B] text-[14px]">Twitter (X)</p>
                 </div>
             </article>
 
-            <article className="flex justify-between items-center">
+            <article className="mt-[48px] flex justify-between items-center">
                 <div>
-                    <p>&copy; 2024 Authority Ledger. All Right Reserved</p>
+                    <p className="text-[#64748B] text-[14px]">&copy; 2024 Authority Ledger. All Right Reserved</p>
                 </div>
                 <div className="flex justify-between items-center gap-[32px]">
-                    <p>Privacy Policy</p>
-                    <p>Terms of Services</p>
+                    <p className="text-[#64748B] text-[14px]">Privacy Policy</p>
+                    <p className="text-[#64748B] text-[14px]">Terms of Services</p>
                 </div>
             </article>
         </footer>
